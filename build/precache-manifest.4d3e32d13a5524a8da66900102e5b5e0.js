c68b2e66300a7cce5fa062faa5411402066106e4
Nyannazz <kantemir.imam@gmail.com>
2020-04-19 00:32:23 +0200

    build

---
+++ build/precache-manifest.4d3e32d13a5524a8da66900102e5b5e0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "257d8bc4129c699b05795bce635bbf09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59fa259946178fe2a9a5",
+    "url": "/static/css/main.7225ceb6.chunk.css"
+  },
+  {
+    "revision": "fafb440190e99c8723b5",
+    "url": "/static/js/2.a22bb152.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.a22bb152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59fa259946178fe2a9a5",
+    "url": "/static/js/main.872441ac.chunk.js"
+  },
+  {
+    "revision": "33024c21441c09617877",
+    "url": "/static/js/runtime-main.a7d9ea7d.js"
+  },
+  {
+    "revision": "71280d69765f4f02f98dccf696d3261a",
+    "url": "/static/media/cat404.71280d69.svg"
+  },
+  {
+    "revision": "a6f20a4fdb20d18cf43f8191f2a07697",
+    "url": "/static/media/movieDark.a6f20a4f.svg"
+  },
+  {
+    "revision": "88cc9994b20de4e767bad43f0b027d65",
+    "url": "/static/media/movieLight.88cc9994.svg"
+  },
+  {
+    "revision": "09631885f7f3d756f398aca66d38c8ff",
+    "url": "/static/media/searchIcon.09631885.svg"
+  }
+]);
